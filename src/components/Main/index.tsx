c823a6268495c1caec3121b08c787100aaa08587
klinkonskydev <klinkonskydev@gmail.com>
2022-01-19 11:17:35 -0300

    add husky and lint-staged with jest

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,7 @@
+const Main = () => (
+  <main>
+    <h1>I am a test</h1>
+  </main>
+);
+
+export default Main;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+type Props = {
+  title: string;
+};
+
+export default function Home({ title = 'React avançado' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,9 +16,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>{title}</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -65,5 +67,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }

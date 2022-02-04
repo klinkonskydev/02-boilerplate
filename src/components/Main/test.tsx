1fdfd952d09a9d4d9b2adcfbe1c28e0135fbe777
klinkonskydev <klinkonskydev@gmail.com>
2022-02-03 21:01:58 -0300

    chore: add plopjs

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -5,7 +5,9 @@ describe('<Main />', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />);
 
-    expect(screen.getByRole('heading', { name: /react avançado/i }));
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument();
     expect(container.firstChild).toMatchSnapshot();
   });
 
